093dd503d9e2d2053701cbe5c2e9869407993560
bye-csavier <xavidp05@gmail.com>
2023-03-14 20:26:20 +0100

    Ok

---
+++ Bearzi Requiem PRJ/src/UI/ElementUI.java
@@ -0,0 +1,45 @@
+package UI;
+
+import extra.Coords;
+import main.MainPanel;
+
+import java.awt.*;
+
+abstract public class ElementUI {
+
+    //=== VARIABLES ====================================================================================================================
+
+    //---- Element Vars ---------------------------------------------------------------------------------------------------------------
+
+    MainPanel mainPanel;
+    Rectangle area;
+    boolean visible;
+
+    //---- Size & Position Vars ---------------------------------------------------------------------------------------------------------------
+
+    public Coords pos;
+    public Coords absPos;
+
+    //---- Animation Vars ---------------------------------------------------------------------------------------------------------------
+
+        //? In Animation
+    protected double inAnimationStart = 0;
+    protected long inAnimationLength;
+        //? General Animation
+    protected double animationStart = 0;
+    protected long animationLength;
+        //? Out Animation
+    protected double outAnimationStart = 0;
+    protected long outAnimationLength;
+
+    protected double transitionProgress = 0;
+    protected double animationProgress = 0;
+
+    protected boolean hover = false;
+
+    //=== CONSTRUCTORS ====================================================================================================================
+
+    //=== FUNCTIONS ====================================================================================================================
+
+//!?!
+}
